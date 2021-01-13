b8cb187cb3ddd1c3d056bdbe39d52203c7af363e
Anjan Koradia <anjankoradia2101@gmail.com>
2021-01-13 11:36:01 +0530

    Popular Category Section & Banner Panal 2 created.

---
+++ JS/App.js
@@ -0,0 +1,11 @@
+// Popular Category Section Slick Slider
+$(".category_slider_container").slick({
+  infinite: true,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow: ".category_prev_btn",
+  nextArrow: ".category_next_btn",
+});
