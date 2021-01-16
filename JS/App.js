f0fdf3f221f7232ccdb60cb60fc3dddaeb885b61
Anjan Koradia <anjankoradia2101@gmail.com>
2021-01-16 11:33:12 +0530

    Hot Deal Section Created.

--- JS/App.js
+++ JS/App.js
@@ -9,3 +9,33 @@ $(".category_slider_container").slick({
   prevArrow: ".category_prev_btn",
   nextArrow: ".category_next_btn",
 });
+
+// Hot Deals Section Slider
+$(".hot_deal_slider").slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow: ".prev_btn",
+  nextArrow: ".next_btn",
+});
+
+let till = new Date("Jan 21, 2023 12:00:00").getTime();
+let timer = setInterval(function () {
+  let currentDate = new Date().getTime();
+  let diffrence = till - currentDate;
+
+  let days = Math.floor(diffrence / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (diffrence % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((diffrence % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diffrence % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
